716a21951180e1110d58fe38a59ac3846896988d
Luuke23 <tomo.zawa.97.1.11@gmail.com>
2026-08-13 15:43:17 +0900

    first commit

---
+++ prediction/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .forms import ImageUploadForm
+import random
+
+def predict(request):
+    if request.method == 'GET':
+        # GETリクエストによるアクセス時の処理を記述
+        form = ImageUploadForm()
+        return render(request, 'home.html', {'form': form})
+    
+    if request.method == 'POST':
+        # POSTリクエストによるアクセス時の処理を記述
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            img_file = form.cleaned_data['image']
+
+            prediction = random.choice(["猫", "犬"])
+            return render(request, 'home.html', {'form': form, 'prediction': prediction})
+        else:
+            form = ImageUploadForm()
+            return render(request, 'home.html', {'form': form})
